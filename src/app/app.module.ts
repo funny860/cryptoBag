c978ea12f88d3ac04cd83c978101386c7dcbf208
funny860 <phanindrarock@gmail.com>
2022-01-19 12:52:26 +0530

    commit on 'master'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CoinsComponent } from './coins/coins.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,7 +25,7 @@ const appRoutes: Routes = [
     LoginComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
